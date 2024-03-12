bb1b0f5f2584f79aba8ae8912113983e9cd2628c
malornh <jivko.ivanov14@gmail.com>
2024-03-12 12:52:09 +0200

    Build DarkMode Switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
+import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/GameHub_logo.webp'
+import ColorModeSwitch from './ColorModeSwitch';
 
 const NavBar = () => {
-  return <HStack>
+  return <HStack justifyContent={'space-between'} padding='10px'>
     <Image src={logo} boxSize='100px' />
-    <Text>NavBar</Text>
+    <ColorModeSwitch />
   </HStack>;
 }
 
