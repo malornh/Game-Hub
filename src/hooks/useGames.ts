5c5fe2953885d4bc5da54d0f1d8c178c5eca688d
malornh <jivko.ivanov14@gmail.com>
2024-03-13 13:04:11 +0200

    Creating game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Games{
+export interface Game{
     id: number,
-    name: string
+    name: string,
+    background_image: string
 }
 
 interface FetchGamesResponse {
@@ -12,7 +13,7 @@ interface FetchGamesResponse {
     results: Games[]
 }
 const useGames = () => {
-    const [games, setGames] = useState<Games[]>([]);
+    const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
     
     useEffect(()=>{
