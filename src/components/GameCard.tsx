c471650d4db4cf55fcb5c30b261443827e2d6296
malornh <jivko.ivanov14@gmail.com>
2024-03-13 17:24:21 +0200

    Creating generic data fetch hook

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props{
 
 const GameCard = ({game}: Props) => {
   return (
-    <Card >
+    <Card height='320px' width='250px' borderRadius='10px' overflow="hidden" >
       <Image src={getCroppedImage(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,7 +4,7 @@ import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 
 const GameGrid = () => {
- const { games, error, isLoading } = useGames();
+ const { data, error, isLoading } = useGames();
  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     return (
@@ -15,8 +15,8 @@ const GameGrid = () => {
           padding={10}
           spacing={10}>
             {isLoading && skeletons.map(s=> <GameCardSkeleton key={s} />)}
-          {games.map((g) => (
-            <GameCard key={g.id} game={g} />
+          {data.map((g) => (
+            <GameCard key={g.id} game={g}  />
           ))}
         </SimpleGrid>
       </>
