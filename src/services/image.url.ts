8444461b4a618e568ad82dffddb21090efc3d20d
malornh <jivko.ivanov14@gmail.com>
2024-03-14 17:46:27 +0200

    Adding noImage.webp to games with no image

--- src/services/image.url.ts
+++ src/services/image.url.ts
@@ -1,5 +1,8 @@
+import noImage from '../assets/noImage.webp'
+
 export const getCroppedImage = (url: string)=>{
-    if(!url) return '';
+    if(!url) return noImage;
+  
     const urlArr = url.split('media/');
     return urlArr[0] + 'media/crop/600/400/' +  urlArr[1];
 }
